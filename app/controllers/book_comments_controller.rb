23d854a0342285f44918a7178cfe1086ada7509e
dym330 <cat.of.attic@gmail.com>
2021-03-02 05:08:02 +0000

    fix comment_controller

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,20 @@
 class BookCommentsController < ApplicationController
 
   def create
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    book = Book.find(params[:book_id])
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_show = Book.find(params[:book_id])
+    @book_comment.book_id = @book_show.id
+    if @book_comment.save
+      redirect_to book_path(@book_show)
+    else
+      render 'books/show'
+    end
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_back(fallback_location: root_path)
+    redirect_to book_path(params[:book_id])
   end
 
   private
