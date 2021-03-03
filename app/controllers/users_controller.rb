48c4ad2f8e2f962ccfe8c1fe643c4c131f10fb45
dym330 <cat.of.attic@gmail.com>
2021-03-03 13:59:01 +0000

    change readme

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,18 +24,18 @@ class UsersController < ApplicationController
       render "edit"
     end
   end
-  
+
   def following
     @title = "Following"
     @user = User.find(params[:id])
-    @users = @user.following.all
+    @users = @user.following
     render "show_follow"
   end
-  
+
   def followers
     @title = "Followers"
     @user = User.find(params[:id])
-    @users = @user.followers.all
+    @users = @user.followers
     render "show_follow"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  
   resources :users, only: [:show,:index,:edit,:update] do
     member do
       get :following, :followers
