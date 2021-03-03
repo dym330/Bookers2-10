57c10f05609ef5a30c691719bdf98e7ecb905439
dym330 <cat.of.attic@gmail.com>
2021-03-03 15:29:15 +0000

    search

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,8 +5,21 @@ class Book < ApplicationRecord
 
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
-	
+
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
 	end
+
+  def self.search(search, how_search)
+	  if how_search == "1"
+	    self.where(['title LIKE ?', "#{search}"])
+	  elsif how_search == "2"
+	    self.where(['title LIKE ?', "#{search}%"])
+	  elsif how_search == "3"
+	    self.where(['title LIKE ?', "%#{search}"])
+	  elsif how_search == "4"
+	    self.where(['title LIKE ?', "%#{search}%"])
+	  end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
@@ -16,24 +16,36 @@ class User < ApplicationRecord
                                   dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  
+
   attachment :profile_image, destroy: false
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
-  
+
   #ユーザーをフォローする
   def follow(other_user)
     following << other_user
   end
-  
+
   #ユーザーのフォロー解除
   def unfollow(other_user)
     active_relationships.find_by(followed_id: other_user.id).destroy
   end
-  
+
   #現在のユーザーがフォローしているか否か
   def following?(other_user)
     following.include?(other_user)
   end
-  
+
+  def self.search(search, how_search)
+	  if how_search == "1"
+	    self.where(['name LIKE ?', "#{search}"])
+	  elsif how_search == "2"
+	    self.where(['name LIKE ?', "#{search}%"])
+	  elsif how_search == "3"
+	    self.where(['name LIKE ?', "%#{search}"])
+	  elsif how_search == "4"
+	    self.where(['name LIKE ?', "%#{search}%"])
+	  end
+  end
+
 end
